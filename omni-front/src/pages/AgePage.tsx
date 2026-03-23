c6463c43cdc3cbb2e92ac76fad019d281db6e80c
SeongYun <tjddbs2065@gmail.com>
2026-03-24 00:34:46 +0900

    ui: shadcn form applying

--- omni-front/src/pages/AgePage.tsx
+++ omni-front/src/pages/AgePage.tsx
@@ -1,8 +1,7 @@
-import { useForm } from "react-hook-form";
+import { Controller, Form, useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
@@ -12,9 +11,15 @@ import { CalendarIcon } from "lucide-react";
 import { useState } from "react";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 
+// form 스키마 구성
 const formSchema = z.object({
-    currentAge: z.string(),
+    username: z.string()
+    .min(3, "Bug title must be at least 5 characters.")
+    .max(32, "Bug title must be at most 32 characters."),
+    birthdate: z.date(),
 });
 
 const AgePage = () => {
@@ -24,10 +29,12 @@ const AgePage = () => {
     const {register, handleSubmit} = useForm<AgeRequest>();
     const { calculate } = useAgeCalc();
 
+    // form 모양 구성
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            currentAge: "",
+            username: "",
+            birthdate: new Date(),
         },
     })
 
@@ -41,45 +48,92 @@ const AgePage = () => {
                 <Card className="flex-1 shadow-md">
                     <CardHeader>입력 조건</CardHeader>
                     <CardContent className="flex flex-col gap-6">
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                        <div className="flex w-full">
-                            <div className="w-20 flex items-start">이름:</div>
-                            <div className="flex-1"><Input placeholder="이름을 입력하세요."></Input> </div>
-                        </div>
-                        <div className="flex flex-1 w-full">
-                            <div className="w-20 flex items-start">생년월일:</div>
-                            <div className="flex-1 flex justify-between">
-                                <div>{date?.toLocaleDateString() || "날짜를 선택하세요"}</div>
-                                <div>
-                                    <Popover open={isOpen} onOpenChange={setIsOpen}>
-                                        <PopoverTrigger>
-                                            <Button><CalendarIcon /></Button>
-                                        </PopoverTrigger> 
-                                        <PopoverContent>
-                                            <Calendar mode="single" captionLayout="dropdown" selected={date} onSelect={(date)=> {setIsOpen(false); setDate(date)}}/>
-                                        </PopoverContent>
-                                    </Popover>
+
+                        <form id="ageForm" onSubmit={form.handleSubmit(onSubmit)}>
+                            
+                            <FieldGroup>
+                                <Controller name="username" control={form.control} render={({field, fieldState}) => (
+                                    <Field data-invalid={fieldState.invalid}>
+                                        <FieldLabel htmlFor="age-form-username">
+                                            이름
+                                        </FieldLabel>
+                                        <Input {...field} id="age-form-username" placeholder="이름을 입력하세요." autoComplete="false"/>
+
+                                        { fieldState.invalid && <FieldError errors={[fieldState.error]} /> }
+                                    </Field>
+                                    )} 
+                                />
+                                <Controller name="birthdate" control={form.control} render={({field, fieldState}) => (
+                                    <Field data-invalid={fieldState.invalid}>
+                                        <FieldLabel htmlFor="age-form-birthdate">
+                                            생년월일
+                                        </FieldLabel>
+                                        <div>
+                                            <Input {...field} id="age-form-birthdate" />
+
+                                            <Popover open={isOpen} onOpenChange={setIsOpen}>
+                                                <PopoverTrigger asChild>
+                                                    <Button type="button" variant="outline" size="icon">
+                                                        <CalendarIcon />
+                                                    </Button>
+                                                </PopoverTrigger> 
+                                                <PopoverContent>
+                                                    <Calendar mode="single" selected={field.value} onSelect={(date)=> {
+                                                        field.onChange(date);
+                                                        setIsOpen(false);
+                                                    }}
+                                                    disabled={(date)=> date > new Date()}
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                        </div>
+                                        
+                                        { fieldState.invalid && <FieldError errors={[fieldState.error]} /> }
+                                    </Field>
+                                    )} 
+                                />
+
+
+                                <div className="flex w-full">
+                                    <div className="w-20 flex items-start">이름:</div>
+                                    <div className="flex-1"><Input placeholder="이름을 입력하세요."></Input> </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="flex">
-                            <div className="w-20 flex items-start">성별: </div>
-                            <div className="flex-1 flex">
-                                <RadioGroup className="flex flex-1 justify-start gap-10" {...register("gender")}>
-                                    <div className="flex items-center gap-3">
-                                        <RadioGroupItem value="male" id="r1" />
-                                        <Label htmlFor="r1">남</Label>
+                                <div className="flex flex-1 w-full">
+                                    <div className="w-20 flex items-start">생년월일:</div>
+                                    <div className="flex-1 flex justify-between">
+                                        <div>{date?.toLocaleDateString() || "날짜를 선택하세요"}</div>
+                                        <div>
+                                            <Popover open={isOpen} onOpenChange={setIsOpen}>
+                                                <PopoverTrigger>
+                                                    <Button><CalendarIcon /></Button>
+                                                </PopoverTrigger> 
+                                                <PopoverContent>
+                                                    <Calendar mode="single" captionLayout="dropdown" selected={date} onSelect={(date)=> {setIsOpen(false); setDate(date)}}/>
+                                                </PopoverContent>
+                                            </Popover>
+                                        </div>
                                     </div>
-                                    <div className="flex items-center gap-3">
-                                        <RadioGroupItem value="female" id="r2" />
-                                        <Label htmlFor="r2">여</Label>
+                                </div>
+                                <div className="flex">
+                                    <div className="w-20 flex items-start">성별: </div>
+                                    <div className="flex-1 flex">
+                                        <RadioGroup className="flex flex-1 justify-start gap-10" {...register("gender")}>
+                                            <div className="flex items-center gap-3">
+                                                <RadioGroupItem value="male" id="r1" />
+                                                <Label htmlFor="r1">남</Label>
+                                            </div>
+                                            <div className="flex items-center gap-3">
+                                                <RadioGroupItem value="female" id="r2" />
+                                                <Label htmlFor="r2">여</Label>
+                                            </div>
+                                        </RadioGroup>
                                     </div>
-                                </RadioGroup>
-                            </div>
-                        </div>
-                        <div>
-                            <Button variant="outline" type="submit">계산</Button>
-                        </div>
+                                </div>
+                                <div>
+                                    <Button variant="outline" type="submit" form="ageForm">계산</Button>
+                                </div>
+                            </FieldGroup>
+
                         </form>
                     </CardContent>
                 </Card>
